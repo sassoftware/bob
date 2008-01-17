d8c7e824de0e85aa7a6039dcb41331bb57f5eff0
Michael Tharp <https://issues.rpath.com/>
2008-01-17 14:43:45 -0500

    Configuration parser

---
+++ bob/config.py
@@ -0,0 +1,37 @@
+#
+# Copyright (c) 2008 rPath, Inc.
+#
+# All rights reserved.
+#
+
+from conary.conarycfg import CfgFlavor
+from conary.lib import cfg
+from conary.lib.cfgtypes import *
+
+class BobTargetSection(cfg.ConfigSection):
+    flavor_set              = CfgString
+    flavor                  = CfgList(CfgFlavor)
+
+class BobTestSection(cfg.ConfigSection):
+    strip                   = CfgInt
+
+class BobConfig(cfg.SectionedConfigFile):
+    # source
+    sourceLabel             = CfgString
+    macro                   = CfgDict(CfgString)
+    mercurial               = CfgDict(CfgString)
+
+    # build
+    shortenGroupFlavors     = (CfgBool, True)
+    tag                     = CfgString
+
+    # custom handling of sections
+    _sectionMap = {'target': BobTargetSection, 'test': BobTestSection}
+
+    def setSection(self, sectionName):
+        for name, typeobj in self._sectionMap.iteritems():
+            if sectionName.startswith(name + ':'):
+                self._addSection(sectionName, typeobj(self))
+                self._sectionName = sectionName
+                return self._sections[sectionName]
+        raise ParseError('Unknown section "%s"' % sectionName)
