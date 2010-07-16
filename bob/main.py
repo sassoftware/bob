a3469b44be1966a0a0906e5e518b043e799bdc5f
Michael Tharp <mtharp@rpath.com>
2010-07-16 14:01:16 -0400

    Default to INFO logging

--- bob/main.py
+++ bob/main.py
@@ -251,7 +251,7 @@ def addRootLogger():
         '%(name)s %(message)s')
     handler.setFormatter(formatter)
     root_log.addHandler(handler)
-    root_log.setLevel(logging.DEBUG)
+    root_log.setLevel(logging.INFO)
 
     # Delete conary's log handler since it puts things on stderr and without
     # any timestamps.
