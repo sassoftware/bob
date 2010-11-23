2fc9499b6f9b11f89c4f79818d3be17e23e532cf
Michael Tharp <mtharp@rpath.com>
2010-11-23 15:52:21 -0500

    Add a workaround for weird conary checkout issues.

--- bob/hg.py
+++ bob/hg.py
@@ -43,34 +43,45 @@ def get_tip(uri):
             'repository %s' % uri)
 
 
-def clone(hgui, uri, cacheDir):
+def updateCache(hgui, uri, remote, cacheDir):
     """Make or update a clone of the given repository."""
+    # Pick a directory to place the clone in. Using the same cache dir as
+    # conary's lookaside means that the recipe load can use it as well.
     dirPath = uri.split('//', 1)[-1]
     dirPath = dirPath.replace('/', '_')
     dirPath = os.path.join(cacheDir, dirPath, 'hg')
 
+    # Create the cache repo if needed.
     if not os.path.isdir(dirPath):
         os.makedirs(dirPath)
     if os.path.isdir(dirPath + '/.hg'):
         repo = hg.repository(hgui, dirPath)
     else:
         repo = hg.repository(hgui, dirPath, create=True)
+
+    # Pull all new remote heads.
     log.debug("Pre-clone: repo %s has heads: %s", dirPath,
             ' '.join(sorted(short(x) for x in repo.heads())))
-    remote = hg.repository(hgui, uri)
     repo.pull(remote, force=True)
     log.debug("Post-clone: repo %s has heads: %s", dirPath,
             ' '.join(sorted(short(x) for x in repo.heads())))
+
+    # Try to work around weird issue that only happens to the conary tree where
+    # .changectx(new_head) fails even though .heads() clearly shows that the
+    # head exists.
+    repo.invalidate()
     return repo
 
 
 def getRecipe(uri, rev, subpath, cacheDir):
     """Get file contents for a subset of the given hg repository."""
+    # Update the local repository cache.
     hgui = ui.ui()
     repo = hg.repository(hgui, uri)
     if not hg.islocal(repo):
-        repo = clone(hgui, uri, cacheDir)
+        repo = updateCache(hgui, uri, repo, cacheDir)
 
+    # Pull out and return the recipe file contents.
     subpath = subpath.strip('/').split('/')
     splen = len(subpath)
 
